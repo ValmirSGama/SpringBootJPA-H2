c4e14ba993884a42d94438c0b2cdc47cfb75484c
Valmir Sales <valmir_salesgama@hotmail.com>
2024-07-26 20:26:22 -0300

    Project created

---
+++ src/test/java/projetoSpringBoot/SpringJPA_H2/SpringJpaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package projetoSpringBoot.SpringJPA_H2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
