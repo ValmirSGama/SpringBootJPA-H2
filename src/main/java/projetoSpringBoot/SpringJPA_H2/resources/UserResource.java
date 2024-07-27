08e20279ec8c92626a12cbcd684713ab6cd3553c
Valmir Sales <valmir_salesgama@hotmail.com>
2024-07-26 22:35:01 -0300

    User entity and resource

---
+++ src/main/java/projetoSpringBoot/SpringJPA_H2/resources/UserResource.java
@@ -0,0 +1,20 @@
+package projetoSpringBoot.SpringJPA_H2.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import projetoSpringBoot.SpringJPA_H2.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	// Método endpoint para acessar os usuários
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
