1eb1b1a3139304734957b0ee24b4b4c7522a262e
Valmir Sales <valmir_salesgama@hotmail.com>
2024-07-27 17:42:11 -0300

    H2 database, test profile, JPA

--- src/main/java/projetoSpringBoot/SpringJPA_H2/entities/User.java
+++ src/main/java/projetoSpringBoot/SpringJPA_H2/entities/User.java
@@ -3,9 +3,19 @@ package projetoSpringBoot.SpringJPA_H2.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable { // "Serializable" possibilita a transformação em cadeia de bytes, para que o objeto trafegue na rede, possa ser gravado em arquivos e etc
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
