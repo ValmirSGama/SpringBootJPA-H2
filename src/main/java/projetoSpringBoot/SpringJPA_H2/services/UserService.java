8c763e2a057279daaaff63135cce463aeb8f74b1
Valmir Sales <valmir_salesgama@hotmail.com>
2024-08-02 13:05:46 -0300

    Exception handling - delete

--- src/main/java/projetoSpringBoot/SpringJPA_H2/services/UserService.java
+++ src/main/java/projetoSpringBoot/SpringJPA_H2/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import projetoSpringBoot.SpringJPA_H2.entities.User;
 import projetoSpringBoot.SpringJPA_H2.repositories.UserRepository;
+import projetoSpringBoot.SpringJPA_H2.services.exceptions.DatabaseException;
 import projetoSpringBoot.SpringJPA_H2.services.exceptions.ResourceNotFoundException;
 
 @Service // Registra como um componete do spring
@@ -34,7 +37,13 @@ public class UserService {
 	
 	// EndPoint para deletar um usuário no banco de dados
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	// EndPoint para atualizar um usuário no banco de dados
---
+++ src/main/java/projetoSpringBoot/SpringJPA_H2/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package projetoSpringBoot.SpringJPA_H2.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
